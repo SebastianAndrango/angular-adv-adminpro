813b33992cc692368e6fd7e9a6b77effaf60b9ff
Ismael Sebastián Andrango Peralbo <51093008+SebastianAndrango@users.noreply.github.com>
2024-05-27 23:10:28 -0500

    Fin seccion 7

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { BreadCrumbsComponent } from './bread-crumbs/bread-crumbs.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,7 +14,8 @@ import { BreadCrumbsComponent } from './bread-crumbs/bread-crumbs.component';
     BreadCrumbsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     SidebarComponent,
